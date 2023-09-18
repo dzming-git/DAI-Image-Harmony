588119eb4d995c36c53faeced0611c58fe51e15d
dzming <dzm_work@163.com>
2023-09-18 15:48:26 +0000

    v0.7 修改了头文件的头部注释

--- include/http_server.h
+++ include/http_server.h
@@ -1,27 +1,10 @@
 /*****************************************************************************
-*  OpenST Basic tool library                                                 *
-*  Copyright (C) 2014 Henry.Wen  renhuabest@163.com.                         *
-*                                                                            *
-*  This file is part of OST.                                                 *
-*                                                                            *
-*  This program is free software; you can redistribute it and/or modify      *
-*  it under the terms of the GNU General Public License version 3 as         *
-*  published by the Free Software Foundation.                                *
-*                                                                            *
-*  You should have received a copy of the GNU General Public License         *
-*  along with OST. If not, see <http://www.gnu.org/licenses/>.               *
-*                                                                            *
-*  Unless required by applicable law or agreed to in writing, software       *
-*  distributed under the License is distributed on an "AS IS" BASIS,         *
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  *
-*  See the License for the specific language governing permissions and       *
-*  limitations under the License.                                            *
+*  Copyright © 2023 - 2023 dzming  dzm_work@163.com.                         *
 *                                                                            *
 *  @file     http_server.h                                                   *
 *  @brief    HTTP服务器                                                       *
 *  @author   dzming                                                          *
 *  @email    dzm_work@163.com                                                *
-*  @license  GNU General Public License (GPL)                                *
 *                                                                            *
 *----------------------------------------------------------------------------*
 *  Remark         : Description                                              *
