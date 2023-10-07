570bbe023a867daf1b9fe667a803f5ad50acb223
dzming <dzm_work@163.com>
2023-10-07 03:19:47 +0000

    v0.31 1.修复已知bug 2.改为浅拷贝 3.添加日志打印 bug：static的错误使用使得只能支持一次摄像头buf的指针读出

--- src/image_loaders/webcamera_hikvision/webcamera_hikvision.cpp
+++ src/image_loaders/webcamera_hikvision/webcamera_hikvision.cpp
@@ -3,18 +3,17 @@
 #include "LinuxPlayM4.h"
 #include <unistd.h>
 #include <chrono>
+#include <iostream>
+
+#define LOG(fmt, ...) printf("[%s : %d] " fmt, __FILE__, __LINE__, ##__VA_ARGS__)
 
 void CALLBACK DecCBFun(int, char* pBuf, int, FRAME_INFO* pFrameInfo, void* videoBufInfo, int) {
-    static int bufLen = (pFrameInfo->nHeight + pFrameInfo->nHeight / 2) * pFrameInfo->nWidth;
-    
-    static bool isFirstRun = true;
-    if (isFirstRun && T_YV12 == pFrameInfo->nType) {
+    if (nullptr == ((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->bufShallowcopy && T_YV12 == pFrameInfo->nType) {
         ((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->h = pFrameInfo->nHeight;
         ((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->w = pFrameInfo->nWidth;
         ((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->bufLen = (pFrameInfo->nHeight + pFrameInfo->nHeight / 2) * pFrameInfo->nWidth;
         ((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->bufShallowcopy = pBuf;
         ((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->bufDeepcopy = new char[((WebcameraHikvisionLoader::VideoBufInfo*)videoBufInfo)->bufLen];
-        isFirstRun = false;
     }
 }
 
@@ -31,6 +30,7 @@ void CALLBACK fRealDataCallBack_V30(LONG, DWORD dwDataType, BYTE* pBuffer, DWORD
 
 WebcameraHikvisionLoader::WebcameraHikvisionLoader(): 
 totalCnt(0), currIdx(0), userId(-1), playOk(false) {
+    std::cout << "Create WebcameraHikvisionLoader" << std::endl;
     initOk = NET_DVR_Init();
     initOk &= NET_DVR_SetConnectTime(2000, 1);
     initOk &= NET_DVR_SetReconnect(10000, true);
@@ -39,13 +39,17 @@ totalCnt(0), currIdx(0), userId(-1), playOk(false) {
 }
 
 WebcameraHikvisionLoader::~WebcameraHikvisionLoader() {
+    std::cout << "Destroy WebcameraHikvisionLoader" << std::endl;
     if (videoBufInfo) {
+        NET_DVR_StopRealPlay(handle);			//关闭预览
+        NET_DVR_Logout(userId);					//注销用户
+        NET_DVR_Cleanup();						//释放SDK资源
         delete videoBufInfo;
     }
 }
 
 bool WebcameraHikvisionLoader::setSource(std::vector<std::string> cameraInfo) {
-    if (4!=cameraInfo.size()) return false;
+    if (4 != cameraInfo.size()) return false;
     std::string address  = cameraInfo[0];
     std::string userName = cameraInfo[1];
     std::string password = cameraInfo[2];
@@ -62,6 +66,7 @@ bool WebcameraHikvisionLoader::setSource(std::vector<std::string> cameraInfo) {
 
     userId = NET_DVR_Login_V40(&pLoginInfo, &lpDeviceInfo);
     if (userId < 0) {
+        LOG("登录摄像头出错, 错误代码：%d\n", (int)NET_DVR_GetLastError());
         NET_DVR_Cleanup();
         return false;
     }
@@ -74,12 +79,13 @@ bool WebcameraHikvisionLoader::setSource(std::vector<std::string> cameraInfo) {
     flag &= PlayM4_SetDecCallBackMend(nPort, DecCBFun, videoBufInfo);  // 设置视频解码回调函数
     flag &= PlayM4_Play(nPort, 0);  // 开始播放
     if (false == flag) {
+        LOG();
+        NET_DVR_Cleanup();
         PlayM4_FreePort(nPort);
-        sleep(1);
         return false;  // 设置解码回调失败，返回
     }
     //启动实时预览，设置实施回调
-    NET_DVR_PREVIEWINFO struPlayInfo;
+    NET_DVR_PREVIEWINFO struPlayInfo = {0};
     struPlayInfo.hPlayWnd = 0; //窗口为空，设备SDK不解码只取流
     struPlayInfo.lChannel = 1; //Channel number 设备通道
     // 码流类型，0-主码流，1-子码流，2-码流3，3-码流4, 4-码流5,5-码流6,7-码流7,8-码流8,9-码流9,10-码流10
@@ -90,6 +96,7 @@ bool WebcameraHikvisionLoader::setSource(std::vector<std::string> cameraInfo) {
     struPlayInfo.bBlocked = 0;
     handle = NET_DVR_RealPlay_V40(userId, &struPlayInfo, fRealDataCallBack_V30, &nPort);
     if (handle < 0) {
+        LOG("实时预览调用出错，错误代码：%d\n", (int)NET_DVR_GetLastError());
         NET_DVR_Logout(userId);
         NET_DVR_Cleanup();
         return false;  // 实时播放失败，返回
@@ -100,6 +107,7 @@ bool WebcameraHikvisionLoader::setSource(std::vector<std::string> cameraInfo) {
         auto end = std::chrono::steady_clock::now();  // 记录结束时间
         auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
         if (duration > 2000) { // 2秒未成功，超时
+            LOG("回调函数启动失败，错误代码：%d\n", (int)NET_DVR_GetLastError());
             NET_DVR_StopRealPlay(handle);
             NET_DVR_Logout(userId);
             NET_DVR_Cleanup();
@@ -125,8 +133,9 @@ cv::Mat WebcameraHikvisionLoader::next() {
     if (nullptr == videoBufInfo->bufShallowcopy) {
         return cv::Mat();
     }
-    memcpy(videoBufInfo->bufDeepcopy, videoBufInfo->bufShallowcopy, videoBufInfo->bufLen);
-    cv::Mat imgYUV420(videoBufInfo->h + videoBufInfo->h / 2, videoBufInfo->w, CV_8UC1, videoBufInfo->bufDeepcopy);
+    // memcpy(videoBufInfo->bufDeepcopy, videoBufInfo->bufShallowcopy, videoBufInfo->bufLen);
+    // cv::Mat imgYUV420(videoBufInfo->h + videoBufInfo->h / 2, videoBufInfo->w, CV_8UC1, videoBufInfo->bufDeepcopy);
+    cv::Mat imgYUV420(videoBufInfo->h + videoBufInfo->h / 2, videoBufInfo->w, CV_8UC1, videoBufInfo->bufShallowcopy);
     cv::Mat imgBGR;
     cv::cvtColor(imgYUV420, imgBGR, cv::COLOR_YUV2BGR_YV12);
     return imgBGR;
